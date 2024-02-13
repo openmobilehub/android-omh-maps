6d93caf457e3b8e432d98ec7d45263ded1e065ae
Maciej Budziński <maciej.budzinski.93@gmail.com>
2024-02-13 10:17:44 +0100

    fix: small fixes

--- apps/maps-sample/src/main/java/com/openmobilehub/android/maps/sample/maps/DebugPolygonHelper.kt
+++ apps/maps-sample/src/main/java/com/openmobilehub/android/maps/sample/maps/DebugPolygonHelper.kt
@@ -28,7 +28,7 @@ object DebugPolygonHelper {
         }
 
         val polygon = omhMap.addPolygon(polygonOptions)
-        polygon?.setTag("Customizable Polygon")
+        polygon?.setTag("Customizable Polygon Press")
 
         return polygon
     }
