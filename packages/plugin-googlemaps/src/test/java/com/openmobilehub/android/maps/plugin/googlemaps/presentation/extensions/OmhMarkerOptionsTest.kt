c4e5689d5a2ba249f39a8043a3d4129c039042bd
artus9033 <artus9033@gmail.com>
2024-02-16 17:11:46 +0100

    test: more extensive tests for OmhMarkerOptionsTest
    Signed-off-by: artus9033 <artus9033@gmail.com>

--- packages/plugin-googlemaps/src/test/java/com/openmobilehub/android/maps/plugin/googlemaps/presentation/extensions/OmhMarkerOptionsTest.kt
+++ packages/plugin-googlemaps/src/test/java/com/openmobilehub/android/maps/plugin/googlemaps/presentation/extensions/OmhMarkerOptionsTest.kt
@@ -16,50 +16,122 @@
 
 package com.openmobilehub.android.maps.plugin.googlemaps.presentation.extensions
 
+import android.graphics.drawable.Drawable
+import com.google.android.gms.maps.model.BitmapDescriptor
 import com.openmobilehub.android.maps.core.presentation.models.OmhCoordinate
 import com.openmobilehub.android.maps.core.presentation.models.OmhMarkerOptions
 import com.openmobilehub.android.maps.plugin.googlemaps.extensions.toMarkerOptions
+import com.openmobilehub.android.maps.plugin.googlemaps.utils.MarkerIconConverter
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkObject
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 internal class OmhMarkerOptionsTest {
     private val omhCoordinate = OmhCoordinate(16.9, 166.0)
-    private val omhMarkerOptions = OmhMarkerOptions().apply {
+    private val omhMarkerOptionsWithIcon = OmhMarkerOptions().apply {
         position = omhCoordinate
-        title = "Marker Title"
+        title = "Marker Title 1"
         isDraggable = true
         anchor = Pair(0.5f, 0.5f)
         alpha = 0.5f
-        snippet = "Marker Snippet"
+        snippet = "Marker Snippet 1"
         isVisible = false
         isFlat = true
         rotation = 87.6f
+        icon = mockk<Drawable>()
+    }
+
+    private val omhMarkerOptionsWithBackgroundColor = OmhMarkerOptions().apply {
+        position = omhCoordinate
+        title = "Marker Title 2"
+        isDraggable = false
+        anchor = Pair(0.2f, 0.73f)
+        alpha = 0.98f
+        snippet = "Marker Snippet 2"
+        isVisible = true
+        isFlat = false
+        rotation = 324.6f
+        backgroundColor = 123
+        icon = null
     }
 
     @Test
-    fun `toMarkerOptions converts OmhMarkerOptions to MarkerOptions`() {
-        val markerOptions = omhMarkerOptions.toMarkerOptions()
+    fun `toMarkerOptions converts OmhMarkerOptions with icon to MarkerOptions`() {
+        mockkObject(MarkerIconConverter)
+        val iconBitmapDescriptor = mockk<BitmapDescriptor>()
+        every { MarkerIconConverter.convertDrawableToBitmapDescriptor(any()) } returns iconBitmapDescriptor
 
-        assertEquals(markerOptions.position.latitude, omhMarkerOptions.position.latitude, 0.0)
-        assertEquals(markerOptions.position.longitude, omhMarkerOptions.position.longitude, 0.0)
+        val markerOptions = omhMarkerOptionsWithIcon.toMarkerOptions()
 
-        assertEquals(markerOptions.title, omhMarkerOptions.title)
+        assertEquals(
+            omhMarkerOptionsWithIcon.position.latitude,
+            markerOptions.position.latitude,
+            0.0
+        )
+        assertEquals(
+            omhMarkerOptionsWithIcon.position.longitude,
+            markerOptions.position.longitude,
+            0.0
+        )
 
-        assertEquals(markerOptions.isDraggable, omhMarkerOptions.isDraggable)
+        assertEquals(omhMarkerOptionsWithIcon.title, markerOptions.title)
 
-        assertEquals(markerOptions.anchorU, omhMarkerOptions.anchor.first)
-        assertEquals(markerOptions.anchorV, omhMarkerOptions.anchor.second)
+        assertEquals(omhMarkerOptionsWithIcon.isDraggable, markerOptions.isDraggable)
 
-        assertEquals(markerOptions.alpha, omhMarkerOptions.alpha)
+        assertEquals(omhMarkerOptionsWithIcon.anchor.first, markerOptions.anchorU)
+        assertEquals(omhMarkerOptionsWithIcon.anchor.second, markerOptions.anchorV)
 
-        assertEquals(markerOptions.snippet, omhMarkerOptions.snippet)
+        assertEquals(omhMarkerOptionsWithIcon.alpha, markerOptions.alpha)
 
-        assertEquals(markerOptions.isVisible, omhMarkerOptions.isVisible)
+        assertEquals(omhMarkerOptionsWithIcon.snippet, markerOptions.snippet)
 
-        assertEquals(markerOptions.isFlat, omhMarkerOptions.isFlat)
+        assertEquals(omhMarkerOptionsWithIcon.isVisible, markerOptions.isVisible)
 
-        assertEquals(markerOptions.rotation, omhMarkerOptions.rotation)
+        assertEquals(omhMarkerOptionsWithIcon.isFlat, markerOptions.isFlat)
 
-        assertEquals(markerOptions.icon, null)
+        assertEquals(omhMarkerOptionsWithIcon.rotation, markerOptions.rotation)
+
+        assertEquals(iconBitmapDescriptor, markerOptions.icon)
+    }
+
+    @Test
+    fun `toMarkerOptions converts OmhMarkerOptions with backgroundColor to MarkerOptions`() {
+        mockkObject(MarkerIconConverter)
+        val colorBitmapDescriptor = mockk<BitmapDescriptor>()
+        every { MarkerIconConverter.convertColorToBitmapDescriptor(any()) } returns colorBitmapDescriptor
+
+        val markerOptions = omhMarkerOptionsWithBackgroundColor.toMarkerOptions()
+
+        assertEquals(
+            omhMarkerOptionsWithBackgroundColor.position.latitude,
+            markerOptions.position.latitude,
+            0.0
+        )
+        assertEquals(
+            omhMarkerOptionsWithBackgroundColor.position.longitude,
+            markerOptions.position.longitude,
+            0.0
+        )
+
+        assertEquals(omhMarkerOptionsWithBackgroundColor.title, markerOptions.title)
+
+        assertEquals(omhMarkerOptionsWithBackgroundColor.isDraggable, markerOptions.isDraggable)
+
+        assertEquals(omhMarkerOptionsWithBackgroundColor.anchor.first, markerOptions.anchorU)
+        assertEquals(omhMarkerOptionsWithBackgroundColor.anchor.second, markerOptions.anchorV)
+
+        assertEquals(omhMarkerOptionsWithBackgroundColor.alpha, markerOptions.alpha)
+
+        assertEquals(omhMarkerOptionsWithBackgroundColor.snippet, markerOptions.snippet)
+
+        assertEquals(omhMarkerOptionsWithBackgroundColor.isVisible, markerOptions.isVisible)
+
+        assertEquals(omhMarkerOptionsWithBackgroundColor.isFlat, markerOptions.isFlat)
+
+        assertEquals(omhMarkerOptionsWithBackgroundColor.rotation, markerOptions.rotation)
+
+        assertEquals(colorBitmapDescriptor, markerOptions.icon)
     }
 }
